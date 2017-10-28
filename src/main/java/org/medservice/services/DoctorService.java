dfb1e9a4f27187e8d94885ba2424f11bb6d024ec
MaksimP <maksimp86@mail.ru>
2017-10-28 18:01:49 +0300

    add methods in DoctorService

--- src/main/java/org/medservice/services/DoctorService.java
+++ src/main/java/org/medservice/services/DoctorService.java
@@ -1,7 +1,12 @@
 package org.medservice.services;
 
+import org.medservice.models.Doctor;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface  DoctorService {
+
+    Doctor findDoctorByUserName(String userName);
+
+    void saveDoctor(Doctor doctor);
 }
--- src/main/java/org/medservice/services/DoctorServiceImpl.java
+++ src/main/java/org/medservice/services/DoctorServiceImpl.java
@@ -1,7 +1,27 @@
 package org.medservice.services;
 
+import org.medservice.models.Doctor;
+import org.medservice.repository.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DoctorServiceImpl implements DoctorService {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @Override
+    public Doctor findDoctorByUserName(String userName) {
+        return doctorRepository.findDoctorByUserName(userName);
+    }
+
+    @Override
+    public void saveDoctor(Doctor doctor) {
+        doctor.setPassword(encoder.encode(doctor.getPassword()));
+    }
 }
